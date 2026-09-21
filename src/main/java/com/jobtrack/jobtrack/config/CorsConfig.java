3f1507d4c445edff0b3649fea09dc920ec0a9794
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-21 16:26:43 +0530

    added cors configuration to solve cors issue

---
+++ src/main/java/com/jobtrack/jobtrack/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.jobtrack.jobtrack.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://jobtrack-frontend-omega.vercel.app"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*");
+    }
+}
